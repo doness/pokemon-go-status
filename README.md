50c0f7e80680580b2d9fd6a3cb59582e1068a880
Alex Markessinis <markea@sage.edu>
2016-07-14 18:49:29 -0400

    updated readme. Server url can be overriden.

--- README.md
+++ README.md
@@ -20,7 +20,42 @@ Or install it yourself as:
 
 ## Usage
 
+To use in a Ruby app:
+
+```ruby
+require 'melonsmasher/pokemon-go-status'
+
+pg = PokemonGoStatus::Status.new
+status = pg.get_server_status
+puts status.inspect
+
+# Possible status objects:
+#
+# "{:exit_code=>0, :description=>\"All good! Go catch'em all!\", :avg_ms=>547, :available=>true}"
+# "{:exit_code=>3, :description=>\"Error! Probably not a good sign, but try again.\", :avg_ms=>-1, :available=>false}"
+# "{:exit_code=>1, :description=>\"Servers are slow. Your mileage may vary.\", :avg_ms=>1381, :available=>true}"
+# "{:exit_code=>2, :description=>\"Servers don't seem to be up.\", :avg_ms=>4557, :available=>false}"
+
+if status.available
+    puts 'Woot Woot! They\'r up!'
+else
+    puts 'Dang, maybe next time.'
+    exit status.exit_code
+end
+
+```
+
+Commandline usage is simple:
+
     $ pgo-status
+    All good! Go catch'em all!
+    Average response time: 547 ms.
+    $ pgo-status
+    Servers are slow. Your mileage may vary.
+    Average response time: 1381 ms.
+    $ pgo-status
+    Servers don't seem to be up.
+    Average response time: 4557 ms.
 
 ## Contributing
 
--- lib/melonsmasher/pokemon-go-status.rb
+++ lib/melonsmasher/pokemon-go-status.rb
@@ -9,31 +9,36 @@ module PokemonGoStatus
         {
             :exit_code => 3,
             :description => 'Error! Probably not a good sign, but try again.',
-            :avg_ms => avg_ms
+            :avg_ms => avg_ms,
+            :available => false
         }
       elsif avg_ms < 800
         {
             :exit_code => 0,
             :description => 'All good! Go catch\'em all!',
-            :avg_ms => avg_ms
+            :avg_ms => avg_ms,
+            :available => true
         }
       elsif avg_ms >= 800 && avg_ms < 3000
         {
             :exit_code => 1,
             :description => 'Servers are slow. Your mileage may vary.',
-            :avg_ms => avg_ms
+            :avg_ms => avg_ms,
+            :available => true
         }
       elsif avg_ms > 3000
         {
             :exit_code => 2,
             :description => 'Servers don\'t seem to be up.',
-            :avg_ms => avg_ms
+            :avg_ms => avg_ms,
+            :available => false
         }
       else
         {
             :exit_code => 3,
             :description => 'Error! Probably not a good sign, but try again.',
-            :avg_ms => avg_ms
+            :avg_ms => avg_ms,
+            :available => false
         }
       end
     end
@@ -52,7 +57,7 @@ module PokemonGoStatus
       total_time / times.count
     end
 
-    def get_server_status url
+    def get_server_status url = 'https://pgorelease.nianticlabs.com/plfe/'
       self.interpret_response_average self.get_server_response_average url
     end
 
